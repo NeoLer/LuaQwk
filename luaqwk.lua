9593d624e9199a7a3e2d37182c63ed3f729eef07
Snoxicle <Snoxicle@gmail.com>
2015-01-24 20:38:16 +0000

    Update luaqwk.lua

--- luaqwk.lua
+++ luaqwk.lua
@@ -1,69 +1,30 @@
-
 --[[
 
-	LuaQwk, a small library of useful functions to speed up coding
+	LuaQwk is a small collection of tools to speed up general coding in Lua
+	 See github.com/Snoxicle/LuaQwk for latest release
+	  Using LuaQwk:
+	 	local LuaQwk = require("luaqwk") -- all of LuaQwk is returned in a table
 
-
-
-	map(table, function)   => iterate over table; set every value to function(value, key)
-	mapT(table, function)  => same as map but for trees of data (tables with tables in them)
-
-
-	process(table, functions...)  => iterate over table; call each function listed with (element, key)
-	processT(table, functions...) => same as process but for trees of data (tables with tables in them)
-
-
-	merge(keep, overwrite) => merge 2 tables: keep the first table, and overwrite the second if there are conflictions
-		Example: merge({1, 2, 3}, {3, 2, 1, 4}) -> {1, 2, 3, 4}
-	
 ]]
 
-
-local function map(tab, func)
-	for k, v in pairs(tab) do
-		tab[k] = func(v, k)
-	end
-end
-
-local function mapT(tab, func)
-	for k, v in pairs(tab) do
-		tab[k] = func(v)
-		if type(v) == "table" then
-			mapT(v, func)
-		end
-	end
-end
+LuaQwk = {}
 
 
 
-local function process(tab, ...)
-	local funcs = {...}
-	for k, v in pairs(tab) do
-		for _, f in pairs(funcs) do
-			f(v, k)
-		end
-	end
-end
-
-local function processT(tab, ...)
-	local funcs = {...}
-	for k, v in pairs(tab) do
-		if type(v) == "table" then
-			for _, f in pairs(funcs) do
-				f(v)
-			end
-			processT(v, ...)
+-- Lets you index strings like you would tables if called
+function LuaQwk.enableArrayStyleStringIndexes()
+	getmetatable('').__index = function(self, i)
+		if type(i) == "string" then
+			return string[i]
 		else
-			for _, f in pairs(funcs) do
-				f(v)
-			end
+			return string.sub(self, i, i)
 		end
 	end
 end
 
 
-
-local function merge(keep, overwrite)
+-- Overwrite/merge keep with overwrite
+function LuaQwk.merge(keep, overwrite)
 	for key, val in pairs(keep) do
 		if type(key) == "string" then
 			overwrite[key] = val
@@ -75,11 +36,72 @@ local function merge(keep, overwrite)
 end
 
 
+-- Traverse over t, call each function with (value, key)
+function LuaQwk.process(t, ...)
+	local funcs = {...}
+	for k, v in pairs(t) do
+		for _, f in pairs(funcs) do
+			f(v, k)
+		end
+	end
+end
 
-return {
-	map         = map,
-	mapT        = mapT,
-	merge       = merge,
-	process     = process,
-	processT    = processT,
-}
+-- Traverse over t, setting each element to f(element)
+function LuaQwk.map(t, f)
+	for k, v in pairs(t) do
+		t[k] = f(v)
+	end
+	return t
+end
+
+-- Create a new table u. For each element of t, set u[element] = f(element)
+function LuaQwk.mapCpy(t, f)
+	local u = {}
+	for k, v in pairs(t) do
+		u[k] = f(v)
+	end
+	return u
+end
+
+
+-- The opposite of unpack()
+function LuaQwk.pack(...)
+	return {...}
+end
+
+
+-- compose(a, b, c) = function(x) return c(b(a(x))) end
+function LuaQwk.compose(...)
+	local fns = {...}
+	return function(...)
+		local result = {...}
+		for _, f in pairs(fns) do
+			result = LuaQwk.pack(f(unpack(result)))
+		end
+		return result
+	end
+end
+
+
+-- Returns true if any argument is nil, else false
+function LuaQwk.anyNil(...)
+	for _, v in pairs({...}) do
+		if v == nil then
+			return true
+		end
+	end
+	return false
+end
+
+-- Calls f unless maybeNil is nil
+function LuaQwk.callUnlessNil(maybeNil, f)
+	if maybeNil ~= nil then
+		return f(maybeNil)
+	else
+		return nil
+	end
+end
+
+
+
+return LuaQwk
